d148f6ce49b5388f4ae020ccd917755c4e6ec284
Eslam <eslam3012mohamed@gmail.com>
2023-10-04 12:16:09 +0300

    add file X-O Game

---
+++ X-O Game.py
@@ -0,0 +1,9 @@
+# Tic-tac-toe, also known as X-O Game, is a two-player game played on a n*n grid. 
+# The game is played by two players who take turns marking the spaces in the grid with X or O. 
+# The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner.
+# If all n*n squares are filled and neither player has three in a row, the game is considered a draw.
+
+from Game import *
+
+game = Game()
+game.StartGame()
